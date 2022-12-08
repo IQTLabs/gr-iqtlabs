b44051eb4b31acbcd5d351b9642511f90d049356
Josh Bailey <josh@vandervecken.com>
2022-12-08 19:31:11 +1300

    Initialize module.

---
+++ include/gnuradio/iqtlabs/api.h
@@ -0,0 +1,18 @@
+/*
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+#ifndef INCLUDED_IQTLABS_API_H
+#define INCLUDED_IQTLABS_API_H
+
+#include <gnuradio/attributes.h>
+
+#ifdef gnuradio_iqtlabs_EXPORTS
+#define IQTLABS_API __GR_ATTR_EXPORT
+#else
+#define IQTLABS_API __GR_ATTR_IMPORT
+#endif
+
+#endif /* INCLUDED_IQTLABS_API_H */
