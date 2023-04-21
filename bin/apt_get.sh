192f13a4636c97c695d502be1b2cab58fb5b25c7
Josh Bailey <josh@vandervecken.com>
2023-04-21 04:03:55 +0000

    Add opencv as a dependency.

--- bin/apt_get.sh
+++ bin/apt_get.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sudo apt-get update && \
-  sudo apt-get -yq install gnuradio-dev build-essential cmake python3-numpy python3-pandas libsndfile-dev libsoapysdr-dev libboost-all-dev libgnuradio-uhd\* libzstd-dev libuhd-dev python3-packaging cppcheck
+  sudo apt-get -yq install gnuradio-dev build-essential cmake python3-numpy python3-pandas libsndfile-dev libsoapysdr-dev libboost-all-dev libgnuradio-uhd\* libzstd-dev libuhd-dev python3-packaging cppcheck libopencv-dev
