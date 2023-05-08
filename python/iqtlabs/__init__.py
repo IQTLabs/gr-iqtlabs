86e948e79ed5908331df35d6f486eb89020e9c08
Josh Bailey <josh@vandervecken.com>
2023-05-09 10:53:14 +1200

    implement vector to json block.

--- python/iqtlabs/__init__.py
+++ python/iqtlabs/__init__.py
@@ -4,10 +4,10 @@
 
 # The presence of this file turns this directory into a Python package
 
-'''
+"""
 This is the GNU Radio IQTLABS module. Place your Python package
 description here (python/__init__.py).
-'''
+"""
 import os
 
 # import pybind11 generated symbols into the iqtlabs namespace
