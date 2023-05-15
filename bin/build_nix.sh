431728e9fc449dfa8afc928a0f58ca0d6b911fd5
Josh Bailey <josh@vandervecken.com>
2023-05-15 21:09:39 +0000

    test.

--- bin/build_nix.sh
+++ bin/build_nix.sh
@@ -2,13 +2,13 @@
 set -e
 python -m venv .venv
 source .venv/bin/activate
-sudo python -m pip install -r codecheck-requirements.txt
+python -m pip install -r codecheck-requirements.txt
 export GRC_BLOCKS_PATH=/var/empty/local/share/gnuradio/grc/blocks
 export CXXFLAGS="-I /var/empty/local/include/ -L /var/empty/local/lib/"
 cppcheck --verbose --language=c++ lib/* && \
   rm -rf build && mkdir build && cd build && cmake .. && make && \
   sed -i "s/\usr\/bin\/python3/usr\/bin\/env python3/g" python/iqtlabs/qa_*sh && \
-  sed -i "s/export LD_LIBRARY_PATH/# export LD_LIBRARY_PATH/g" python/iqtlabs/qa_*sh && \
+  cat python/iqtlabs/qa_*sh && \
   make test ARGS="--verbose --timeout 180" && sudo make install && sudo ldconfig && cd .. && \
   cd test_flow_graphs && rm -rf test && mkdir test && cd test && testdir=$(pwd) && \
   for g in ../*grc ; do
