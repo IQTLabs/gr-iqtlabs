78bb1169e8aeaf870b3751b68573bec6fee30b39
Josh Bailey <josh@vandervecken.com>
2023-09-12 03:25:59 +0000

    pin glslang version to prevent build breaks, faster make build.

--- bin/build_test_nix.sh
+++ bin/build_test_nix.sh
@@ -5,4 +5,4 @@ export PATH=/var/empty/local/bin:$PATH
 cppcheck --verbose --language=c++ --inline-suppr lib/* && \
   pip3 install --user -r codecheck-requirements.txt && \
   ./bin/clonedeps.sh && \
-  rm -rf build && mkdir build && cd build && cmake .. && make && sudo make install && sudo ldconfig && sudo PYTHONPATH=$HOME/.local/lib/python3.10/site-packages:$PYTHONPATH make test ARGS="--verbose --timeout 180" && cd ..
+  rm -rf build && mkdir build && cd build && cmake .. && make -j $(nproc) && sudo make install && sudo ldconfig && sudo PYTHONPATH=$HOME/.local/lib/python3.10/site-packages:$PYTHONPATH make test ARGS="--verbose --timeout 180" && cd ..
