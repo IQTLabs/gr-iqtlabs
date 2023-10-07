2840cdb4fdd7f4290a7505a15e65410a189cfccf
Josh Bailey <josh@vandervecken.com>
2023-10-07 21:33:30 +0000

    Use g++ 11 on 20.04

--- bin/apt_get_39.sh
+++ bin/apt_get_39.sh
@@ -2,11 +2,18 @@
 # https://wiki.gnuradio.org/index.php?title=UbuntuInstall#Install_Dependencies
 set -e
 sudo apt-get -y update && \
+    sudo DEBIAN_FRONTEND=noninteractive apt-get -y install \
+     software-properties-common && \
+     sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test && \
+     sudo apt-get -y update && \
+     sudo apt-get install g++-11 gcc-11 && \
+     sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 10 && \
+     sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-11 10
+sudo apt-get -y update && \
     sudo apt-get -y install \
 	 cmake \
 	 cppcheck \
 	 doxygen \
-	 g++ \
 	 gir1.2-gtk-3.0 \
 	 git \
 	 libboost-all-dev \
@@ -51,5 +58,5 @@ sudo apt-get -y update && \
 	 swig \
     && \
     git clone --recursive https://github.com/gnuradio/volk && cd volk && mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release -DPYTHON_EXECUTABLE=/usr/bin/python3 .. && make -j "$(nproc)" && sudo make install && sudo ldconfig && cd ../.. && \
-    git clone https://github.com/gnuradio/gnuradio -b maint-3.9 && cd gnuradio && mkdir build && cd build && CMAKE_CXX_STANDARD=17 cmake -DENABLE_DEFAULT=OFF -DENABLE_GRC=ON -DENABLE_PYTHON=ON -DENABLE_GNURADIO_RUNTIME=ON -DENABLE_GR_BLOCKS=ON -DENABLE_GR_FFT=ON -DENABLE_GR_FILTER=ON -DENABLE_GR_ANALOG=ON -DENABLE_GR_NETWORK=ON .. && make -j "$(nproc)" && sudo make install && sudo ln -s /usr/local/lib/python3/dist-packages/* /usr/local/lib/python3.*/dist-packages && sudo ldconfig && cd ../.. &&
+    git clone https://github.com/gnuradio/gnuradio -b maint-3.9 && cd gnuradio && mkdir build && cd build && CXXFLAGS=-fext-numeric-literals CMAKE_CXX_STANDARD=17 cmake -DENABLE_DEFAULT=OFF -DENABLE_GRC=ON -DENABLE_PYTHON=ON -DENABLE_GNURADIO_RUNTIME=ON -DENABLE_GR_BLOCKS=ON -DENABLE_GR_FFT=ON -DENABLE_GR_FILTER=ON -DENABLE_GR_ANALOG=ON -DENABLE_GR_NETWORK=ON .. && make -j "$(nproc)" && sudo make install && sudo ln -s /usr/local/lib/python3/dist-packages/* /usr/local/lib/python3.*/dist-packages && sudo ldconfig && cd ../.. &&
     python3 -c "import gnuradio"
