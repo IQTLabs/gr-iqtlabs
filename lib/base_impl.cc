11fa4aef680b691b0b1253c421e34e63ebc69a64
Josh Bailey <josh@vandervecken.com>
2024-08-05 00:29:37 +0000

    freq.

--- lib/base_impl.cc
+++ lib/base_impl.cc
@@ -286,7 +286,7 @@ sigmf_record_t base_impl::create_sigmf(const std::string &source_file,
       basename(source_file.c_str());
   capture.get<sigmf::capture_details::DescrT>().gain = gain;
   if (frequency) {
-    capture.get<sigmf::core::DescrT>().frequency = 0;
+    capture.get<sigmf::core::DescrT>().frequency = frequency;
   }
   record.captures.emplace_back(capture);
   return record;
