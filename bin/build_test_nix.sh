1f91fab8e109fe01d2ba9b57995bba54e5d31883
Josh Bailey <josh@vandervecken.com>
2024-08-06 01:44:59 +0000

    sigmf_validate.

--- bin/build_test_nix.sh
+++ bin/build_test_nix.sh
@@ -4,5 +4,6 @@ export CXXFLAGS="-I /var/empty/local/include/ -L /var/empty/local/lib/"
 export PATH=/var/empty/local/bin:$PATH
 cppcheck --verbose --language=c++ --inline-suppr --suppress=preprocessorErrorDirective --suppress=syntaxError --force lib/* && \
   pip3 install --user -r codecheck-requirements.txt && \
+  sudo pip3 install --user -r codecheck-requirements.txt && \
   ./bin/clonedeps.sh && \
   rm -rf build && mkdir build && cd build && cmake .. && make -j $(nproc) && sudo make install && sudo ldconfig && sudo PYTHONPATH=$HOME/.local/lib/python3.10/site-packages:$PYTHONPATH make test ARGS="--verbose --timeout 180" && cd ..
